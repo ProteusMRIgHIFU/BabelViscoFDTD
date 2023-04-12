b1674da3edc5e3defb1ab408efd36cde774c2337
spichardo <samuel.pichardo@ucalgary.ca>
2023-04-11 22:05:41 -0600

    Minor release to refresh pip that had an outdate file for OpenCL

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import sysconfig
 
 dir_path =path.dirname(os.path.realpath(__file__))+os.sep
 
-version = '1.0.0'
+version = '1.0.0-1'
 
 npinc=np.get_include()+os.sep+'numpy'
 # Filename for the C extension module library
