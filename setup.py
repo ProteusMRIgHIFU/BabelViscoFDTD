aee6d3ef41cd1b6c849f583e173a5eb95588951a
spichardo <samuel.pichardo@ucalgary.ca>
2025-10-21 22:08:10 -0600

    FIX: Wrong sizes for arrays for MLX - upgrade version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import sysconfig
 
 dir_path =path.dirname(os.path.realpath(__file__))+os.sep
 
-version = '1.1.2'
+version = '1.1.3'
 
 npinc=np.get_include()+os.sep+'numpy'
 # Filename for the C extension module library
