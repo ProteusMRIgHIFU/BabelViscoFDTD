c94f674b7fde043931b665744cd34722ec888474
spichardo <samuel.pichardo@ucalgary.ca>
2025-12-27 11:58:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import sysconfig
 
 dir_path =path.dirname(os.path.realpath(__file__))+os.sep
 
-version = '1.2.0'
+version = '1.2.1'
 
 npinc=np.get_include()
 # Filename for the C extension module library
